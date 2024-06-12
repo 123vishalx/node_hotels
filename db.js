f3bf3eefb3a5bc2e25155574fd567d28d5c9e7c0
123vishalx <162402810+123vishalx@users.noreply.github.com>
2024-06-12 16:46:08 +0530

    server online

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb+srv://vishalshiwani2003:vishal123cluster0.xceanfb.mongodb.net/'
+// const mongoURL = ''
+// mongodb+srv://vishalshiwani2003:vishal123@cluster0.xceanfb.mongodb.net/
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
